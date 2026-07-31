3cd85c6f82aab4c18d9dda187f6b1cc8c0fd667c
alexahinojosa26 <alexahinojosa26@gmail.com>
2026-07-31 11:12:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showTab(id, btn) {
+  document.querySelectorAll('.tab-content').forEach(t => t.classList.remove('active'));
+  document.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
+  document.getElementById('tab-' + id).classList.add('active');
+  btn.classList.add('active');
+}
